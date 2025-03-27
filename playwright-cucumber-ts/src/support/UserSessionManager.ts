eb9365c31cd411ee912cdf3333f251a180c1bb4c
realworldred <paulrooney60@gmail.com>
2025-03-27 08:01:51 +0000

    updates to usersessionManager

--- playwright-cucumber-ts/src/support/UserSessionManager.ts
+++ playwright-cucumber-ts/src/support/UserSessionManager.ts
@@ -1,12 +1,13 @@
 import { Page } from '@playwright/test';
 import { UserSession } from '../types';
+import { chromium } from 'playwright';
 
 const sessions = new Map<string, UserSession>();
 let activeUser: UserSession | null = null;
 
 export const userSessionManager = {
-	addUser: (alias: string, page: Page): void => {
-		const session = { alias, page };
+	addUser: async (alias: string): Promise<void> => {
+		const session = await createSession(alias);
 		sessions.set(alias, session);
 	},
 
@@ -24,4 +25,40 @@ export const userSessionManager = {
 		const session = sessions.get(alias);
 		return session ? session.page : null;
 	},
+
+	awaitLogin: async (
+		alias: string,
+		loginTask: (page: Page) => Promise<void>
+	): Promise<void> => {
+		const session = sessions.get(alias);
+		if (!session) {
+			throw new Error(`User with alias "${alias}" does not exist.`);
+		}
+		await loginTask(session.page);
+	},
+
+	runTasksForUsers: async (
+		aliases: string[],
+		taskMap: Map<string, (page: Page) => Promise<void>>
+	): Promise<void> => {
+		const tasks = aliases.map(async (alias) => {
+			const session = sessions.get(alias);
+			if (!session) {
+				throw new Error(`User with alias "${alias}" does not exist.`);
+			}
+			const task = taskMap.get(alias);
+			if (!task) {
+				throw new Error(`No task found for user with alias "${alias}".`);
+			}
+			await task(session.page);
+		});
+		await Promise.all(tasks);
+	},
+};
+
+export const createSession = async (alias: string): Promise<UserSession> => {
+	const browser = await chromium.launch();
+	const context = await browser.newContext();
+	const page = await context.newPage();
+	return { alias, page };
 };
